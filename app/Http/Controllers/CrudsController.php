233743805b8a5b121d0aadbc59cdfd2d67bce583
Mamadou <bam76531@gmail.com>
2020-06-08 22:46:44 +0200

    initial commit

---
+++ app/Http/Controllers/CrudsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Crud;
+
+class CrudsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Crud::latest()->paginate(5);
+        return view("index", compact("data"))
+                ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'last_name'=>'required',
+            'last_name' => 'required',
+            'image' => 'required'
+        ]);
+        $image = $request->file('image');
+        $new_image =rand() . "." . $image->getClientOriginalExtension();
+        $image->move(public_path('images'), $new_name);
+        $form_data= array(
+            'first_name'=> $request->first_name,
+            'last_name'=> $request->last_name,
+            'image'  => $new_image
+        );
+
+        Crud::create($form_data);
+        return view(redirect('crud')->with('success', 'Data added Successfully.'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
